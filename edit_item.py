296c573a16b5aa6ab3084ac4498f0094f4edd393
Gerald Joy Banez <geraldjoyb@gmail.com>
2024-03-25 11:33:50 +0800

    Added the Edit item function and print on the table

---
+++ edit_item.py
@@ -0,0 +1,92 @@
+from PyQt6.QtWidgets import QLabel, QPushButton, QLineEdit, QMainWindow, QApplication, QComboBox, QDateTimeEdit
+from PyQt6 import uic
+from database import Database
+import mysql.connector
+from warning_dialog import Warning
+from datetime import datetime
+
+class EditItems(QMainWindow):
+    
+    def __init__(self):
+        super(EditItems, self).__init__()
+
+        uic.loadUi("Ui/add_item_window.ui", self)
+
+        self.warning = Warning()
+
+        self.barcode = self.findChild(QLineEdit, "barcode")
+        self.item_name = self.findChild(QLineEdit, "item_name")
+        self.quality = self.findChild(QComboBox, "quality_box")
+        self.datetime = self.findChild(QDateTimeEdit, "datetime_added")
+        self.save_button = self.findChild(QPushButton, "save_button")
+        self.label = self.findChild(QLabel, "label")
+
+        self.quality.addItem("Good")
+        self.quality.addItem("Bad")
+        self.quality.addItem("Broken")
+
+        self.label.setText("Edit User")
+
+        self.save_button.clicked.connect(self.saveEdit)
+
+    def editItem(self, creds):
+
+        self.item = creds[0]
+        self.qual = creds[1]
+        self.brcd = creds[2]
+        self.date = creds[3]
+        self.stat = creds[4]
+        self.borrower = creds[5]
+
+        self.curr_datetime = datetime.strptime(self.date, "%Y-%m-%d %H:%M:%S")
+
+        self.item_name.setText(self.item)
+        self.quality.setCurrentText(self.qual)
+        self.barcode.setText(self.brcd)
+        self.datetime.setDateTime(self.curr_datetime)
+
+    def saveEdit(self):
+
+        if not self.verifyItem():
+            return
+        
+        db = Database()
+        connection = db.connect()
+        cursor = connection.cursor()
+
+        if self.item_name.text() != self.item or self.quality.currentText() != self.qual or self.datetime.text() != self.date:
+            try:
+                cursor.execute(f"SELECT item_id FROM lnhsis.items WHERE item_name = '{self.item}'")
+            except mysql.connector.Error as err:
+                print("Error: ", err)
+
+            item_id = cursor.fetchall()
+
+            try:
+                cursor.execute(f"UPDATE lnhsis.items SET item_name = '{self.item_name.text()}', quality = '{self.quality.currentText()}', date_added = '{self.datetime.text()}' WHERE item_id = {item_id[0][0]}")
+            except mysql.connector.Error as err:
+                print("Error:", err)
+
+            connection.commit()
+
+        db.close()
+        self.close()
+        self.warning.setWarning("Edit Saved!")
+        self.warning.show()
+
+        
+    def verifyItem(self):
+
+        if not self.item_name.text():
+            self.warning.setWarning("Item Name Field is empty!")
+            self.warning.show()
+            return
+        
+        return True
+
+        
+if __name__ == "__main__":
+    app = QApplication([])
+    window = EditItems()
+    window.show()
+    app.exec()
--- item_management_window.py
+++ item_management_window.py
@@ -1,7 +1,7 @@
 from PyQt6.QtGui import QCloseEvent, QStandardItem, QStandardItemModel
-from PyQt6.QtWidgets import QPushButton, QMainWindow, QApplication, QLineEdit,QTableView, QHeaderView
+from PyQt6.QtWidgets import QPushButton, QMainWindow, QApplication, QLineEdit,QTableView, QHeaderView, QDialog
 from PyQt6 import uic
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import pyqtSignal, QModelIndex
 from add_item import AddItem
 from database import Database
 import mysql.connector
@@ -50,7 +50,7 @@ class Item_Mngmnt(QMainWindow):
         connection.close()
         
         self.model = QStandardItemModel(0,6)
-        self.model.setHorizontalHeaderLabels(['Item','Quality','Barcode', 'Date Added','Status','Borrower','',''])
+        self.model.setHorizontalHeaderLabels(['Item','Quality','Barcode', 'Date Added','Status','Borrower','','',''])
 
         items = []
         for row in results:
@@ -68,18 +68,118 @@ class Item_Mngmnt(QMainWindow):
             ]
             items.append(QStandardItem("Edit"))
             items.append(QStandardItem("Delete"))
+            items.append(QStandardItem("Print"))
             self.model.appendRow(items)
 
         self.view = self.main_table
         self.view.setModel(self.model)
-        self.view.setColumnWidth(0, 175)
+        self.view.setColumnWidth(0, 150)
         self.view.setColumnWidth(1, 75)
         self.view.setColumnWidth(2, 250)
         self.view.setColumnWidth(3, 150)
+        self.view.setColumnWidth(5, 150)
+        self.view.setColumnWidth(6, 50)
+        self.view.setColumnWidth(7, 50)
+        self.view.setColumnWidth(8, 50)
         self.main_table.selectionModel().selectionChanged.connect(self.tableItemClicked)
 
-    def tableItemClicked(self):
-        return
+    def tableItemClicked(self, item:QModelIndex):
+
+        selected_item = ""
+
+        for index in item.indexes():
+            selected_item = index
+
+        selected_item_data = selected_item.data()
+
+        itemname_index = self.model.index(selected_item.row(),0)
+        quality_index = self.model.index(selected_item.row(),1)
+        barcode_index = self.model.index(selected_item.row(),2)
+        date_index = self.model.index(selected_item.row(),3)
+        status_index = self.model.index(selected_item.row(),4)
+        borrower_index = self.model.index(selected_item.row(),5)
+
+        selected_itemname = self.model.data(itemname_index)
+        selected_quality = self.model.data(quality_index)
+        selected_barcode = self.model.data(barcode_index)
+        selected_date = self.model.data(date_index)
+        selected_status = self.model.data(status_index)
+        selected_borrower = self.model.data(borrower_index)
+
+        if selected_item_data == "Edit":
+
+            creds = [selected_itemname, selected_quality, selected_barcode, selected_date, selected_status, selected_borrower]
+
+            from edit_item import EditItems
+
+            self.edit_item_window = EditItems()
+            self.edit_item_window.editItem(creds)
+            self.edit_item_window.show()
+            
+        if selected_item_data == "Delete":
+            
+            from warning_dialog import Warning
+
+            self.warning = Warning()
+
+            self.warning.setWarning(f"Delete {selected_itemname} and their data?")
+
+            res = self.warning.exec()
+
+            if res == QDialog.DialogCode.Accepted:
+                db = Database()
+
+                connection = db.connect()
+                cursor = connection.cursor()
+
+                try:
+                    cursor.execute(f"SELECT item_id FROM lnhsis.items WHERE barcode = '{selected_barcode}'")
+                except mysql.connector.Error as err:
+                    print("Error 1: ", err)
+
+                result = cursor.fetchall()
+
+
+                for item in result:
+                    try:
+                        cursor.execute(f"DELETE FROM lnhsis.items WHERE item_id = {item[0]}")
+                    except mysql.connector.Error as err:
+                        print("Error 2: ", err)
+                        connection.rollback()
+                    connection.commit()
+
+                db.close()
+                self.warning.setWarning(f"{selected_itemname} was Deleted Successfully")
+                self.warning.show()
+                self.loadItems()
+
+            else:
+                self.warning.setWarning("Deletion Cancelled!")
+                self.warning.show()
+
+        if selected_item_data == "Print":
+
+
+
+            from printbarcode import PrintBarcode
+            import barcode, os
+            from barcode.writer import ImageWriter
+
+            filepath = f'Barcodes/{selected_itemname}_{selected_barcode}'
+
+            if os.path.exists(filepath) and os.path.isfile(filepath):
+                PrintBarcode.printCode(self,filepath)
+                return
+
+            Code128 = barcode.get_barcode_class('code128')
+            code128 = Code128(selected_barcode, writer = ImageWriter())
+
+            code128.save(filepath, options={'write_text' : True})
+
+            from printbarcode import PrintBarcode
+
+            PrintBarcode.printCode(self,filepath)
+            
 
 
 if __name__ == "__main__":
