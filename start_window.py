74b9ce30cb38cf6cfc515183ccba7a307c912bfe
Gerald Joy Banez <geraldjoyb@gmail.com>
2024-04-10 00:41:18 +0800

    Made the program ask for Admin prevs.

--- start_window.py
+++ start_window.py
@@ -1,65 +1,78 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QLineEdit, QFrame, QPushButton, QDialog
-from PyQt6 import uic
-import mysql.connector, datetime
-from menu_window import Main_menu
-from user_management_window import User_Mngmnt
-from UNPW_pop import Ui_incUNPW_warning
-from database import Database
+import ctypes, sys
 
-class Login_Window(QMainWindow):
-    def __init__(self):
-        super(Login_Window,self).__init__()
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
-        uic.loadUi("Ui/Log_in-window.ui", self)
+if is_admin():
+    from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QLineEdit, QFrame, QPushButton, QDialog
+    from PyQt6 import uic
+    import mysql.connector, datetime, ctypes, sys
+    from menu_window import Main_menu
+    from user_management_window import User_Mngmnt
+    from UNPW_pop import Ui_incUNPW_warning
+    from database import Database
 
-        self.username_input = self.findChild(QLineEdit, "username_input")
-        self.pass_input = self.findChild(QLineEdit, "pass_input")
-        self.login_button = self.findChild(QPushButton, "log_in_button")
+    class Login_Window(QMainWindow):
+        def __init__(self):
+            super(Login_Window,self).__init__()
 
-        self.login_button.clicked.connect(self.login)
+            uic.loadUi("Ui/Log_in-window.ui", self)
 
-        self.main_menu_window = Main_menu()
-        self.usr_mngmt_win = User_Mngmnt()
-        self.main_menu_window.closed_to_login.connect(self.show)
+            self.username_input = self.findChild(QLineEdit, "username_input")
+            self.pass_input = self.findChild(QLineEdit, "pass_input")
+            self.login_button = self.findChild(QPushButton, "log_in_button")
 
-    def login(self):
-        db = Database()
+            self.login_button.clicked.connect(self.login)
 
-        connection = db.connect()
-        cursor = connection.cursor()
-        
-        given_cred = [self.username_input.text(), self.pass_input.text()]
-
-        if db.verify_login(given_cred):
-
-            action = "Logged in"
-            current_datetime = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+            self.main_menu_window = Main_menu()
+            self.usr_mngmt_win = User_Mngmnt()
+            self.main_menu_window.closed_to_login.connect(self.show)
             
-            try:
-                cursor.execute(f"INSERT INTO lnhsis.logs (user, action, log_date) VALUES ('{given_cred[0]}', '{action}', '{current_datetime}' )")
-            except mysql.connector.Error as err:
-                print("Error: ", err)
-            connection.commit()
-            
-            db.close()
+        def login(self):
+            db = Database()
 
-            self.username_input.setText("")
-            self.pass_input.setText("")
-            self.main_menu_window.show()
-            self.hide()
-        else:
-            db.close()
-        
-            self.username_input.setText("")
-            self.pass_input.setText("")
-            self.window = QDialog()
-            self.ui = Ui_incUNPW_warning()
-            self.ui.setupUi(self.window)
-            self.window.show()
-        
-        
-if __name__ == "__main__":
-    app = QApplication([])
-    Ui_Login_window = Login_Window()
-    Ui_Login_window.show()
-    app.exec()
+            connection = db.connect()
+            cursor = connection.cursor()
+            
+            given_cred = [self.username_input.text(), self.pass_input.text()]
+
+            if db.verify_login(given_cred):
+
+                action = "Logged in"
+                current_datetime = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+                
+                try:
+                    cursor.execute(f"INSERT INTO lnhsis.logs (user, action, log_date) VALUES ('{given_cred[0]}', '{action}', '{current_datetime}' )")
+                except mysql.connector.Error as err:
+                    print("Error: ", err)
+                connection.commit()
+                
+                db.close()
+
+                self.username_input.setText("")
+                self.pass_input.setText("")
+                self.main_menu_window.show()
+                self.hide()
+            else:
+                db.close()
+            
+                self.username_input.setText("")
+                self.pass_input.setText("")
+                self.window = QDialog()
+                self.ui = Ui_incUNPW_warning()
+                self.ui.setupUi(self.window)
+                self.window.show()
+            
+            
+    if __name__ == "__main__":
+        app = QApplication([])
+        Ui_Login_window = Login_Window()
+        Ui_Login_window.show()
+        app.exec()
+
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
